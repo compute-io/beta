3df93c88180e51104dda66a3f1d38c67f7c98d17
kgryte <kgryte@gmail.com>
2015-07-21 20:46:31 -0700

    [TESTS] add array-typedarray tests.

---
+++ test/test.array-typedarray.js
@@ -0,0 +1,84 @@
+/* global describe, it, require */
+'use strict';
+
+// MODULES //
+
+var // Expectation library:
+	chai = require( 'chai' ),
+
+	// Deep close to:
+	deepCloseTo = require( './utils/deepcloseto.js' ),
+
+	// Module to be tested:
+	beta = require( './../lib/array-typedarray.js' );
+
+
+// VARIABLES //
+
+var expect = chai.expect,
+	assert = chai.assert;
+
+
+// TESTS //
+
+describe( 'array-typedarray beta', function tests() {
+
+	it( 'should export a function', function test() {
+		expect( beta ).to.be.a( 'function' );
+	});
+
+	it( 'should throw an error if provided arrays of unequal length', function test() {
+		expect( foo ).to.throw( Error );
+		function foo() {
+			beta( [1,2], [1,2], new Int32Array( 3 ) );
+		}
+	});
+
+	it( 'should evaluate the beta function', function test() {
+		var expected,
+			actual,
+			x, y;
+
+		x = [ 1, 2, 3, 4 ];
+		y = new Int8Array( [ 1, 2, 3, 4 ] );
+
+		actual = new Array( x.length );
+		actual = beta( actual, x, y );
+
+		expected = [
+			1,
+			0.1666666666666667,
+			0.03333333333333333,
+			0.007142857142857144
+		];
+
+		assert.isTrue( deepCloseTo( actual, expected, 1e-7 ) );
+	});
+
+	it( 'should return an empty array if provided an empty array', function test() {
+		assert.deepEqual( beta( [], [], [] ), [] );
+	});
+
+	it( 'should handle non-numeric values by setting the element to NaN', function test() {
+		var expected,
+			actual,
+			x, y;
+
+		x = [ true, null, [], {} ];
+		y = new Int8Array( [ 1, 2, 3, 4 ] );
+
+		actual = new Array( x.length );
+		actual = beta( actual, x, y );
+
+		expected = [ NaN, NaN, NaN, NaN ];
+		assert.deepEqual( actual, expected );
+
+		actual = new Int32Array( x.length );
+		actual = beta( actual, x, y );
+
+		// Integer arrays cannot represent NaN:
+		expected = [ 0, 0, 0, 0 ];
+		assert.deepEqual( actual, expected );
+	});
+
+});
